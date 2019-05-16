54861780ef1d4818f2a70f894f3338663b7e4ea6
Aiman Mallah <aimanmallah@gmail.com>
2019-05-16 18:16:16 +0100

    added secret

--- config/environment.js
+++ config/environment.js
@@ -1,4 +1,4 @@
-const secret = 'secret'
+const secret = process.env.SECRET || 'thisisadifficultpassword'
 const port = process.env.PORT || 4000
 const dbUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/cabins-db'
 module.exports = { secret, port, dbUri }
