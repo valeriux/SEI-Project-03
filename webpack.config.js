20010d5f14c1cd3e7c176baf81ebe35613c7c5be
Aiman Mallah <aimanmallah@gmail.com>
2019-05-17 11:20:25 +0100

    Deploying...

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const webpack = require('webpack')
 
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 const Dotenv = require('dotenv-webpack')
 
@@ -55,6 +56,9 @@ module.exports = {
       filename: 'index.html',
       inject: 'body'
     }),
+    new CopyWebpackPlugin([
+      { from: 'src/images', to: 'images' }
+    ]),
     new Dotenv()
   ]
 }
